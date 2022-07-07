9c54fd14e3ab862bca81095afbc59b2365613dd6
Niklas Röttger <niklas.roettger@outlook.de>
2022-07-07 19:20:34 +0200

    add single page appli

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,3 +1,53 @@
 export default function navbar() {
-  const navbarItems = document.querySelectorAll('[data-js="navbar-element"]');
+  const indexPage = document.querySelector('[data-js="index-page"]');
+  const bookmarkPage = document.querySelector('[data-js="bookmark-page"]');
+  const createPage = document.querySelector('[data-js="create-page"]');
+  const profilePage = document.querySelector('[data-js="profile-page"]');
+
+  const pages = [indexPage, bookmarkPage, createPage, profilePage];
+
+  const indexNavbar = document.querySelector('[data-js="index-navbar"]');
+  const bookmarkNavbar = document.querySelector('[data-js="bookmark-navbar"]');
+  const createNavbar = document.querySelector('[data-js="create-navbar"]');
+  const profileNavbar = document.querySelector('[data-js="profile-navbar"]');
+
+  const navbarItems = [
+    indexNavbar,
+    bookmarkNavbar,
+    createNavbar,
+    profileNavbar,
+  ];
+
+  function hideContent() {
+    pages.forEach((page) => {
+      page.classList.add("hidden");
+    });
+    navbarItems.forEach((item) => {
+      item.classList.remove("navbar--wrapper-active");
+    });
+  }
+
+  indexNavbar.addEventListener("click", () => {
+    hideContent();
+    indexNavbar.classList.add("navbar--wrapper-active");
+    indexPage.classList.remove("hidden");
+  });
+
+  bookmarkNavbar.addEventListener("click", () => {
+    hideContent();
+    bookmarkNavbar.classList.add("navbar--wrapper-active");
+    bookmarkPage.classList.remove("hidden");
+  });
+
+  createNavbar.addEventListener("click", () => {
+    hideContent();
+    createNavbar.classList.add("navbar--wrapper-active");
+    createPage.classList.remove("hidden");
+  });
+
+  profileNavbar.addEventListener("click", () => {
+    hideContent();
+    profileNavbar.classList.add("navbar--wrapper-active");
+    profilePage.classList.remove("hidden");
+  });
 }
--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 import bookmark from "./components/bookmark/bookmark.js";
 import card from "./components/card/card.js";
+import navbar from "./components/navbar/navbar.js";
 
 card();
 
 bookmark();
+
+navbar();
