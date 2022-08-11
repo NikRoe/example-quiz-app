cac290a915ccfd0570155045e62913c3029d15f7
Niklas Röttger <niklas.roettger@outlook.de>
2022-08-11 09:56:18 +0200

    add js createelement and db.js with array of objects

---
+++ db.js
@@ -0,0 +1,23 @@
+export const questionCards = [
+  {
+    id: 1,
+    questionText:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora?",
+    answerText: "Lorem, ipsum dolor sit amet consectetur adipisicing elit",
+    tags: ["HTML", "Git", "Markdown", "CSS"],
+  },
+  {
+    id: 2,
+    questionText:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora?",
+    answerText: "Lorem, ipsum dolor sit amet consectetur adipisicing elit",
+    tags: ["HTML", "JavaScript", "CSS"],
+  },
+  {
+    id: 3,
+    questionText:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora?",
+    answerText: "Lorem, ipsum dolor sit amet consectetur adipisicing elit",
+    tags: ["JavScript", "Git", "Markdown", "CSS"],
+  },
+];
